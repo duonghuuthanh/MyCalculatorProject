f2253fb1f2f50280e43a3245b839c7e7b39aca75
HV0909 <dhthanhqa@gmail.com>
2019-09-20 11:17:39 +0700

    Add unit test with ms unit

---
+++ Calculation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculation
+    {
+        private int a, b;
+        public Calculation(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public int execute(string phepToan)
+        {
+            int kq = 0;
+
+            switch (phepToan)
+            {
+                case "+":
+                    kq = this.a + this.b;
+                    break;
+                case "-":
+                    kq = this.a - this.b;
+                    break;
+                case "*":
+                    kq = this.a * this.b;
+                    break;
+                case "/":
+                    if (b == 0)
+                        new DivideByZeroException("Loi chia 0");
+                    kq = this.a / this.b;
+                    break;
+            }
+
+            return kq;
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -24,8 +24,21 @@ namespace Calculator
             {
                 int a = int.Parse(textBox1.Text);
                 int b = int.Parse(textBox2.Text);
-                int d = a + b;
-                label1.Text = d.ToString();
+
+                Calculation cl = new Calculation(a, b);
+                label1.Text = cl.execute("+").ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text) &&
+                !string.IsNullOrEmpty(textBox2.Text))
+            {
+                int a = int.Parse(textBox1.Text);
+                int b = int.Parse(textBox2.Text);
+                Calculation cl = new Calculation(a, b);
+                label1.Text = cl.execute("/").ToString();
             }
         }
     }
